09055fa2fe9da704545b253f1893eca8736661c0
Tanavmaji21 <348840182@gapps.yrdsb.ca>
2023-11-21 09:01:29 -0500

    First edit, moved and added object.

---
+++ Stars.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Stars here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Stars extends World
+{
+
+    /**
+     * Constructor for objects of class Stars.
+     * 
+     */
+    public Stars()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        Elephant tanav = new Elephant();
+        addObject(tanav,50,100);
+        
+    }
+}
